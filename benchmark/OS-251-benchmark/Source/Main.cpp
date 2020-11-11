79d5a55dc0a9514bf5ab8b9c6def5c19bd432dd0
utokusa <utokusa@gmail.com>
2020-11-12 00:22:28 +0900

    Add benchmark for the synthesizer engine

--- benchmark/OS-251-benchmark/Source/Main.cpp
+++ benchmark/OS-251-benchmark/Source/Main.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic startup code for a JUCE application.
+    Benchmark for synthesizer engine
 
   ==============================================================================
 */
@@ -9,114 +9,43 @@
 #include "../../../Source/SynthEngine.h"
 
 #include <JuceHeader.h>
+#include <algorithm>
 #include <benchmark/benchmark.h>
-#include <iostream>
-
-//#define DEV_BENCHMARK
 
 //==============================================================================
-double sampleRate = 44100.0;
-int numSample = 512;
-int numChannel = 2;
+// Constants
 
-#ifdef DEV_BENCHMARK
-int main (int argc, char* argv[])
-{
-    std::cout << "Synth engine start." << std::endl;
+constexpr double SAMPLE_RATE = 44100.0;
+constexpr double MAX_TIME_SEC = 2.0;
+constexpr int NUM_SAMPLE = static_cast<int> (SAMPLE_RATE) * static_cast<int> (MAX_TIME_SEC);
+constexpr int NUM_CHANNEL = 2;
+// First byte of a midi message
+constexpr int NOTE_ON = 0x90;
 
-    SynthEngine synthEngine;
-    SynthParams& synthParams (SynthParams::getInstance());
+// Notes
+constexpr int C1 = 0x3c;
+[[maybe_unused]] constexpr int CS1 = 0x3d;
+constexpr int D1 = 0x3e;
+[[maybe_unused]] constexpr int DS1 = 0x3f;
+constexpr int E1 = 0x40;
+constexpr int F1 = 0x41;
+[[maybe_unused]] constexpr int FS1 = 0x42;
+constexpr int G1 = 0x43;
+[[maybe_unused]] constexpr int GS1 = 0x44;
+constexpr int A1 = 0x45;
+[[maybe_unused]] constexpr int AS1 = 0x46;
+constexpr int B1 = 0x47;
 
-    std::atomic<float> sinGain (0.5f);
-    std::atomic<float> squareGain (0.5f);
-    std::atomic<float> sawGain (0.5f);
-    std::atomic<float> subSquareGain (0.5f);
+// Velocity
+constexpr int VEL_100 = 0x64; // 100 in decimal
+constexpr int VEL_OFF = 0x00;
 
-    std::atomic<float> attack (0.5f);
-    std::atomic<float> decay (0.5f);
-    std::atomic<float> sustain (0.5f);
-    std::atomic<float> release (0.5f);
-
-    std::atomic<float> frequency (0.5f);
-    std::atomic<float> resonance (0.5f);
-    std::atomic<float> filterEnvelope (0.5f);
-
-    // Oscillator parameters
-    OscillatorParams* const oscillatorParams = synthParams.oscillator();
-    oscillatorParams->setSinGainPtr (&sinGain);
-    oscillatorParams->setSquareGainPtr (&squareGain);
-    oscillatorParams->setSawGainPtr (&sawGain);
-    oscillatorParams->setSubSquareGainPtr (&subSquareGain);
-
-    // Envelop parameters
-    EnvelopeParams* const envelopeParams = synthParams.envelope();
-    envelopeParams->setAttackPtr (&attack);
-    envelopeParams->setDecayPtr (&decay);
-    envelopeParams->setSustainPtr (&sustain);
-    envelopeParams->setReleasePtr (&release);
-
-    // Filter parameters
-    FilterParams* const filterParams = synthParams.filter();
-    filterParams->setFrequencyPtr (&frequency);
-    filterParams->setResonancePtr (&resonance);
-    filterParams->setFilterEnvelopePtr (&filterEnvelope);
-
-    synthEngine.prepareToPlay (0, sampleRate);
-
-    std::cout << "Synth engine is initialized." << std::endl;
-
-    std::cout << "Create input." << std::endl;
-
-    juce::AudioBuffer<float> outputAudio (numChannel, numSample);
-
-    // juce::MidiMessage
-    // MidiMessage (int byte1, int byte2, int byte3, double timeStamp = 0) noexcept;
-    juce::MidiMessage message;
-
-    // juce::MidiBuffer
-    // void addEvent (const MidiMessage& midiMessage, int sampleNumber);
-    juce::MidiBuffer inputMidi;
-
-    std::cout << "Start process." << std::endl;
-    synthEngine.renderNextBlock (outputAudio, inputMidi, 0, numSample);
-
-    std::cout << "Finish." << std::endl;
-
-    return 0;
-}
-#endif
+//==============================================================================
 
 class SynthEngineFixture : public benchmark::Fixture
 {
 public:
-    SynthEngine synthEngine;
-    SynthParams& synthParams = { SynthParams::getInstance() };
-
-    std::atomic<float> sinGain = { 0.5f };
-    std::atomic<float> squareGain = { 0.5f };
-    std::atomic<float> sawGain = { 0.5f };
-    std::atomic<float> subSquareGain = { 0.5f };
-
-    std::atomic<float> attack = { 0.5f };
-    std::atomic<float> decay = { 0.5f };
-    std::atomic<float> sustain = { 0.5f };
-    std::atomic<float> release = { 0.5f };
-
-    std::atomic<float> frequency = { 0.5f };
-    std::atomic<float> resonance = { 0.5f };
-    std::atomic<float> filterEnvelope = { 0.5f };
-
-    juce::AudioBuffer<float> outputAudio = { numChannel, numSample };
-
-    // juce::MidiMessage
-    // MidiMessage (int byte1, int byte2, int byte3, double timeStamp = 0) noexcept;
-    juce::MidiMessage message;
-
-    // juce::MidiBuffer
-    // void addEvent (const MidiMessage& midiMessage, int sampleNumber);
-    juce::MidiBuffer inputMidi;
-
-    void SetUp (const ::benchmark::State& state)
+    void SetUp (::benchmark::State& state) override
     {
         // Oscillator parameters
         OscillatorParams* const oscillatorParams = synthParams.oscillator();
@@ -138,11 +67,77 @@ public:
         filterParams->setResonancePtr (&resonance);
         filterParams->setFilterEnvelopePtr (&filterEnvelope);
 
-        synthEngine.prepareToPlay (0, sampleRate);
+        synthEngine.prepareToPlay (0, SAMPLE_RATE);
+
+        for (const auto& note : notes)
+        {
+            inputMidiBuffer.addEvent (note, timeSecToSample (note.getTimeStamp(), SAMPLE_RATE, NUM_SAMPLE));
+        }
     }
 
-    void TearDown (const ::benchmark::State& state)
+    void TearDown (::benchmark::State& state) override
     {
+    }
+
+    void render()
+    {
+        synthEngine.renderNextBlock (outputAudio, inputMidiBuffer, 0, NUM_SAMPLE);
+    }
+
+    //==============================================================================
+private:
+    // Private member variables
+    SynthEngine synthEngine;
+    SynthParams& synthParams = { SynthParams::getInstance() };
+
+    std::atomic<float> sinGain = { 0.5f };
+    std::atomic<float> squareGain = { 0.5f };
+    std::atomic<float> sawGain = { 0.5f };
+    std::atomic<float> subSquareGain = { 0.5f };
+
+    std::atomic<float> attack = { 0.5f };
+    std::atomic<float> decay = { 0.5f };
+    std::atomic<float> sustain = { 0.5f };
+    std::atomic<float> release = { 0.5f };
+
+    std::atomic<float> frequency = { 0.5f };
+    std::atomic<float> resonance = { 0.5f };
+    std::atomic<float> filterEnvelope = { 0.5f };
+
+    juce::AudioBuffer<float> outputAudio = { NUM_CHANNEL, NUM_SAMPLE };
+
+    // juce::MidiMessage
+    // MidiMessage (int byte1, int byte2, int byte3, double timeStamp = 0) noexcept;
+    double deltaTime = 0.1; // The unit is [sec]
+    std::vector<juce::MidiMessage> notes = {
+        { NOTE_ON, C1, VEL_100, 0.0 * deltaTime },
+        { NOTE_ON, C1, VEL_OFF, 2.0 * deltaTime },
+        { NOTE_ON, D1, VEL_100, 1.0 * deltaTime },
+        { NOTE_ON, D1, VEL_OFF, 3.0 * deltaTime },
+        { NOTE_ON, E1, VEL_100, 2.0 * deltaTime },
+        { NOTE_ON, E1, VEL_OFF, 4.0 * deltaTime },
+        { NOTE_ON, F1, VEL_100, 3.0 * deltaTime },
+        { NOTE_ON, F1, VEL_OFF, 5.0 * deltaTime },
+        { NOTE_ON, G1, VEL_100, 4.0 * deltaTime },
+        { NOTE_ON, G1, VEL_OFF, 6.0 * deltaTime },
+        { NOTE_ON, A1, VEL_100, 5.0 * deltaTime },
+        { NOTE_ON, A1, VEL_OFF, 7.0 * deltaTime },
+        { NOTE_ON, B1, VEL_100, 6.0 * deltaTime },
+        { NOTE_ON, B1, VEL_OFF, 8.0 * deltaTime },
+    };
+
+    // juce::MidiBuffer
+    // void addEvent (const MidiMessage& midiMessage, int sampleNumber);
+    juce::MidiBuffer inputMidiBuffer;
+
+    //==============================================================================
+    // Private method
+
+    static int timeSecToSample (double timeSec, double sampleRate, int numSample)
+    {
+        int ret = static_cast<int> (timeSec * sampleRate);
+        ret = std::clamp (ret, 0, numSample);
+        return ret;
     }
 };
 
@@ -151,10 +146,8 @@ BENCHMARK_F (SynthEngineFixture, render)
 {
     for (auto _ : state)
     {
-        synthEngine.renderNextBlock (outputAudio, inputMidi, 0, numSample);
+        render();
     }
 }
 
-#ifndef DEV_BENCHMARK
 BENCHMARK_MAIN();
-#endif
