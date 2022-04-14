0058caaa53ac495422c1c1bf0fea4c0094d93765
utokusa <utokusa@gmail.com>
2022-04-15 08:42:04 +0900

    Implement own synth engine

--- src/synth/SynthEngine.h
+++ src/synth/SynthEngine.h
@@ -12,6 +12,7 @@
 #include "../dsp/DspCommon.h"
 #include "../dsp/Hpf.h"
 #include "../dsp/IPositionInfo.h"
+#include "../dsp/JuceAudioBuffer.h"
 #include "../dsp/Lfo.h"
 #include "../dsp/MasterVolume.h"
 #include "SynthParams.h"
@@ -20,6 +21,104 @@
 
 namespace onsen
 {
+class OnsenSynth
+{
+public:
+    OnsenSynth (SynthParams* const synthParams, Lfo* const _lfo)
+        : params (synthParams),
+          lfo (_lfo),
+          voice (params, lfo),
+          pitchBendValue (8192 /*no pitchbend*/),
+          hpf (params->hpf(), 2),
+          chorus(),
+          masterVolume (synthParams->master()) {}
+    void setCurrentPlaybackSampleRate (double sampleRate)
+    {
+        lfo->setCurrentPlaybackSampleRate (sampleRate);
+        voice.setCurrentPlaybackSampleRate (sampleRate);
+        chorus.setCurrentPlaybackSampleRate (sampleRate);
+        hpf.setCurrentPlaybackSampleRate (sampleRate);
+    }
+    void setSamplesPerBlock (int samplesPerBlock)
+    {
+        lfo->setSamplesPerBlock (samplesPerBlock);
+    }
+
+    void renderNextBlock (juce::AudioBuffer<float>& outputAudio, const juce::MidiBuffer& inputMidi, int startSample, int numSamples)
+    {
+        if (! inputMidi.getNumEvents())
+        {
+            renderHelper (outputAudio, startSample, numSamples);
+            return;
+        }
+        int firstEventTime = inputMidi.getFirstEventTime();
+        renderHelper (outputAudio, startSample, std::max (0, firstEventTime - startSample));
+        auto it = inputMidi.begin();
+        while (it != inputMidi.end())
+        {
+            auto metadata = *it;
+            int curSample = metadata.samplePosition;
+            while (it != inputMidi.end() && curSample == (*it).samplePosition)
+            {
+                // consume event
+                auto midiMsg = (*it).getMessage();
+                int midiStatusByte = (int) (*(midiMsg.getRawData()));
+                if (midiMsg.isNoteOff())
+                {
+                    voice.stopNote (0.0, true);
+                    lfo->noteOff();
+                    std::cout << "Midi note off..." << std::endl;
+                }
+                else if (midiMsg.isNoteOn())
+                {
+                    int intVelocity = static_cast<int> (midiMsg.getVelocity());
+                    flnum velocity = static_cast<flnum> (intVelocity) / 127.0;
+                    voice.startNote (midiMsg.getNoteNumber(), velocity, nullptr, pitchBendValue);
+                    lfo->noteOn();
+                    std::cout << "Note on... note number:" << midiMsg.getNoteNumber() << std::endl;
+                    std::cout << "velocity:" << (int) (midiMsg.getVelocity()) << std::endl;
+                }
+                else if (midiMsg.isAllNotesOff())
+                {
+                    voice.stopNote (0.0, true);
+                    lfo->allNoteOff();
+                    std::cout << "Midi all notes off..." << std::endl;
+                }
+                else if (midiMsg.isPitchWheel())
+                {
+                    pitchBendValue = midiMsg.getPitchWheelValue();
+                    voice.pitchWheelMoved (pitchBendValue);
+                }
+                it++;
+            }
+            // render audio
+            int numSamplesOfSection = it == inputMidi.end() ? startSample + numSamples - curSample : (*it).samplePosition - curSample;
+            renderHelper (outputAudio, curSample, numSamplesOfSection);
+        }
+    }
+
+private:
+    SynthParams* const params;
+    Lfo* const lfo;
+    FancySynthVoice voice;
+    Hpf hpf;
+    Chorus chorus;
+    MasterVolume masterVolume;
+    int pitchBendValue;
+
+    void renderHelper (juce::AudioBuffer<float>& outputAudio, int startSample, int numSamples)
+    {
+        JuceAudioBuffer outputAudioBuffer (&outputAudio);
+        lfo->renderLfo (startSample, numSamples);
+        lfo->renderLfoSync (startSample, numSamples);
+        voice.renderNextBlock (outputAudio, startSample, numSamples);
+        hpf.render (&outputAudioBuffer, startSample, numSamples);
+        if (params->chorus()->getChorusOn())
+            chorus.render (&outputAudioBuffer, startSample, numSamples);
+        masterVolume.render (&outputAudioBuffer, startSample, numSamples);
+    }
+};
+
 //==============================================================================
 class FancySynth : public juce::Synthesiser
 {
@@ -71,10 +170,10 @@ public:
           lfo (synthParams->lfo(), positionInfo),
           synth (synthParams, &lfo)
     {
-        for (auto i = 0; i < 4; ++i)
-            synth.addVoice (new FancySynthVoice (synthParams, &lfo));
+        // for (auto i = 0; i < 4; ++i)
+        //     synth.addVoice (new FancySynthVoice (synthParams, &lfo));
 
-        synth.addSound (new FancySynthSound());
+        // synth.addSound (new FancySynthSound());
     }
 
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate)
@@ -93,36 +192,36 @@ public:
 
     void changeNumberOfVoices (int num)
     {
-        int numVoices = synth.getNumVoices();
-        if (num == numVoices)
-            return;
-        if (num > numVoices)
-            addNumberOfVoices (num - numVoices);
-        else
-            subNumberOfVoices (numVoices - num);
+        // int numVoices = synth.getNumVoices();
+        // if (num == numVoices)
+        //     return;
+        // if (num > numVoices)
+        //     addNumberOfVoices (num - numVoices);
+        // else
+        //     subNumberOfVoices (numVoices - num);
 
-        assert (num == synth.getNumVoices());
+        // assert (num == synth.getNumVoices());
     }
 
 private:
     SynthParams* const synthParams;
     IPositionInfo* positionInfo;
     Lfo lfo;
-    FancySynth synth;
+    OnsenSynth synth;
     juce::MidiMessageCollector midiCollector;
 
     void addNumberOfVoices (int num)
     {
-        for (auto i = 0; i < num; ++i)
-            synth.addVoice (new FancySynthVoice (synthParams, &lfo));
+        //  for (auto i = 0; i < num; ++i)
+        //      synth.addVoice (new FancySynthVoice (synthParams, &lfo));
     }
 
     void subNumberOfVoices (int num)
     {
-        int numVoices = synth.getNumVoices();
-        assert (numVoices >= num);
-        for (auto i = 0; i < num; ++i)
-            synth.removeVoice (numVoices - 1 - i);
+        // int numVoices = synth.getNumVoices();
+        // assert (numVoices >= num);
+        // for (auto i = 0; i < num; ++i)
+        //     synth.removeVoice (numVoices - 1 - i);
     }
 };
 } // namespace onsen
